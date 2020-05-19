a4e4249994063ede73a5cf02298195fed2ef68bc
Joe <joseph.as.peterson@gmail.com>
2020-05-19 14:19:58 -0600

    get a connection and read an event

--- shure.go
+++ shure.go
@@ -1,18 +1,39 @@
 package shure
 
+import (
+	"bufio"
+	"net"
+	"time"
+)
+
 type AudioControl struct {
+	Address string
 }
 
 type Connection struct {
-	// similar to net.Conn
+	Conn net.Conn
 }
 
-//GetConnection will form a connection with a shure audio device
+//GetConnection will form a connection with a device at Address
 func (s *AudioControl) GetConnection() (*Connection, error) {
-	return nil, nil
+	connection, err := net.DialTimeout("tcp", s.Address+":2202", time.Second*3)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Connection{
+		Conn: connection,
+	}, nil
 }
 
 //ReadEvent will read an event from a shure audio device
-func (c *Connection) ReadEvent() error {
-	return nil
+func (c *Connection) ReadEvent() (string, error) {
+	reader := bufio.NewReader(c.Conn)
+
+	data, err := reader.ReadString('>') // does readstring block?
+	if err != nil {
+		return "", err
+	}
+
+	return data, nil
 }
